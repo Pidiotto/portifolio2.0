b42e5799ca3dca47e484c4b12c8c33f507dc2c19
Pidiotto <wagner.nardes@gmail.com>
2024-02-07 22:11:34 -0300

    Atualização 3

--- script/script.js
+++ script/script.js
@@ -1,3 +1,14 @@
-function myFunction(x) {
+function clickMenu(x) {
     x.classList.toggle("change");
+
+    // menu de sobreposição
+    var overlayMenu = document.getElementById("overlayMenu");
+    overlayMenu.style.display = (overlayMenu.style.display === "block") ? "none" : "block";
 }
+
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
